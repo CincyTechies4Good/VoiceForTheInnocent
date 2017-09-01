d04a0690173e8ca35fb018e88c587434179fda37
boggsey <eric@mlemedia.com>
2017-09-01 17:18:33 -0400

    Adjusting the Resources model. Ditching NA in the json and removing website column and search filter

--- src/app/models/Resource.ts
+++ src/app/models/Resource.ts
@@ -1,9 +1,19 @@
 export class Resource {
     resourceType: string;
+    name: string;
+    hasLocation: boolean;
+    isMandatoryReporter: boolean;
+    addresses: Address[]; // user can have one or more addresses
+    website: string;
+    phoneEmergency: string;
+    phoneNonEmergency: string;
+    email: string;
+}
+
+export class Address {
+    streetAddress: string;
+    streetAddress2: string;
     city: string;
     state: string;
-    name: string;
-    website: string;
-    phone: string;
-    email: string;
+    zipCode: number;
 }
